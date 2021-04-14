311571077998cf597c53d8ef24450a446140c3bc
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2021-04-14 16:04:46 -0600

    Add files via upload

--- PrintingTest.java
+++ PrintingTest.java
@@ -9,9 +9,22 @@ public class PrintingTest {
   
   //may need to rewrite part of class to do unit tests of this
   //test manufacturers list printout
-
-  //test order form with 1 item
-
-  //test order form of multiple items
-
+  @Test
+  public void testManufacturers(){
+    String suggested = {"Office Furnishings", "Chairs R Us", "Furniture Goods", "Fine Office Supplies"};
+    String expected = "Order cannot be fulfilled based on current inventory.Suggested manufactures are Office Furnishings, Chairs R Us, Furniture Goods and Fine Office Supplies";
+    Printing p = new Printing("Kneeling chair, 1", null, suggested);
+    String result = p.writeFile();
+    assertEquals(expected,result);
+  }
+  //test order form
+@Test
+  public void testWorking(){
+    String ids = {"200", "C8138", "C9890"};
+    String expected = "Furniture order Form ->\n\nFaculty Name: \nContact: \nDate: \n\n\nOriginal Request: " +"Mesh chair, 1\n\n\n"+ 
+    "Items Ordered:\nID: C8138\nID: C9890\n\n\nTotal Price: $200";
+    Printing p = new Printing("Mesh chair, 1", ids, null);
+    String result = p.writeFile();
+    assertEquals(expected,result);
+  }
 }
